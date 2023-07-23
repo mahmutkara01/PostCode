9a504b1cadc30121249cb84417b3073cdd69faf0
mahmutkara01 <bilgimahmutkara@gmail.com>
2023-07-23 14:05:28 +0300

    Initial Commit

---
+++ PostCode/PostCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostCodeApp.swift
+//  PostCode
+//
+//  Created by mahmut kara on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PostCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
